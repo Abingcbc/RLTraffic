f90866902cd9495101f57f284453e9f5920b2c6f
Abingcbc <19990626.love@163.com>
2020-01-31 20:18:21 +0800

    react frame

---
+++ src/components/datav/index.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react'
+
+import {
+    BorderBox2,
+    BorderBox1,
+    BorderBox3,
+} from '@jiaminghi/data-view-react'
+
+import LeftChart1 from './LeftChart1'
+import LeftChart2 from './LeftChart2'
+import LeftChart3 from './LeftChart3'
+
+import CenterCmp from './CenterCmp'
+
+import './index.less'
+import render from "less/lib/less/render";
+
+const style = {
+    width: '120px',
+    height: '50px',
+    lineHeight: '50px',
+    textAlign: 'center',
+    marginLeft: '200px',
+}
+
+export default class DataV extends Component {
+    render() {
+        let algorithm = [
+            {
+                aName: '算法1'
+            },
+            {
+                aName: '算法2'
+            },
+            {
+                aName: '算法3'
+            },
+            {
+                aName: '算法4'
+            },
+        ];
+        let mapList = algorithm.map((item, index) => {
+            return (
+                <BorderBox3 className="rmctc-left-container">
+                    <CenterCmp key={index} {...item}/>
+                </BorderBox3>)
+        });
+        return (
+            <div id="data-view">
+                <div className="main-header">
+                    <div className="mh-left"></div>
+                    <div className="mh-middle">车载人工智能调度决策支持系统</div>
+                    <div className="mh-right">
+                        <BorderBox2 style={style}>同济大学</BorderBox2>
+                    </div>
+                </div>
+                <BorderBox1 className="main-container">
+                    <BorderBox3 className="left-chart-container">
+                        <LeftChart1/>
+                        <LeftChart2/>
+                        <LeftChart3/>
+                    </BorderBox3>
+                    <div className="right-main-container">
+                        <div className="rmc-top-container">
+                            {mapList}
+                        </div>
+                    </div>
+                </BorderBox1>
+            </div>
+        )
+    }
+}
