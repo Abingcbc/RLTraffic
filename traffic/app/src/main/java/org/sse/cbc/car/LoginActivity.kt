e395cf7d177ef6f96aae15b95ef757d7548c6c59
Abingcbc <19990626.love@163.com>
2020-03-17 18:47:02 +0800

    refactor

--- traffic/app/src/main/java/org/sse/cbc/car/LoginActivity.kt
+++ traffic/app/src/main/java/org/sse/cbc/car/LoginActivity.kt
@@ -1,12 +1,65 @@
 package org.sse.cbc.car
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.Call
+import okhttp3.Response
+import shem.com.materiallogin.DefaultLoginView
+import shem.com.materiallogin.DefaultRegisterView
+import shem.com.materiallogin.MaterialLoginView
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
+
+    private val preferences by lazy { SharedPreferencesUtils(this) }
+
+    private fun View.yum(text: String, length: Int = Snackbar.LENGTH_SHORT): Snackbar {
+        return Snackbar.make(this, text, length)
+    }
+
+    private fun onRegister(user: String, password: String) {
+        preferences.username = user
+        preferences.isLogin = true
+        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+    }
+
+    private fun onLogin(user: String, password: String) {
+        preferences.username = user
+        preferences.isLogin = true
+        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        if (preferences.isLogin){
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }
+        ((login as MaterialLoginView).registerView as DefaultRegisterView).setListener { registerUser, registerPass,
+                                                                                         registerPassRep ->
+            val passRep = registerPassRep.editText!!.text.toString()
+            val pass = registerPass.editText!!.text.toString()
+            val user = registerUser.editText!!.text.toString()
+            if (passRep != pass) {
+                login.yum("两次密码不同！").show()
+            } else if (pass == "" || user == "") {
+                login.yum("无效的用户名或密码！").show()
+            } else {
+                onRegister(user, pass)
+            }
+        }
+        ((login as MaterialLoginView).loginView as DefaultLoginView).setListener { loginUser, loginPass ->
+            val pass = loginPass.editText!!.text.toString()
+            val user = loginUser.editText!!.text.toString()
+            if (user == "" || pass == "") {
+                login.yum("无效的用户名或密码！").show()
+            } else {
+                onLogin(user, pass)
+            }
+        }
     }
 }
--- traffic/app/src/main/java/org/sse/cbc/car/PositionActivity.java
+++ traffic/app/src/main/java/org/sse/cbc/car/PositionActivity.java
@@ -1,11 +1,17 @@
 package org.sse.cbc.car;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
@@ -22,11 +28,21 @@ public class PositionActivity extends AppCompatActivity {
     private MapView mapView;
     private RecyclerView recyclerView;
     private OrderAdapter orderAdapter;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener;
+
+    private TextView speedTextView;
+    private TextView altitudeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_position);
+
+        speedTextView = findViewById(R.id.speed);
+        altitudeTextView = findViewById(R.id.altitude);
 
         mapView = findViewById(R.id.position_map);
         mapView.onCreate(savedInstanceState);
@@ -40,17 +56,56 @@ public class PositionActivity extends AppCompatActivity {
         aMap.setMyLocationStyle(myLocationStyle);
         aMap.setMyLocationEnabled(true);
 
-        recyclerView = findViewById(R.id.order);
-        recyclerView.setItemAnimator(new SlideInUpAnimator());
-        orderAdapter = new OrderAdapter(this, new CopyOnWriteArrayList<String>());
-        ScaleInAnimationAdapter animationAdapter = new ScaleInAnimationAdapter(orderAdapter);
-        animationAdapter.setFirstOnly(false);
-        recyclerView.setAdapter(animationAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                if (aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                        speedTextView.setText("当前速度："+ aMapLocation.getSpeed() +"米/秒");
+                        altitudeTextView.setText("当前海拔："+aMapLocation.getAltitude()+"米");
+                    }else {
+                        //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                        Log.e("AmapError","location Error, ErrCode:"
+                                + aMapLocation.getErrorCode() + ", errInfo:"
+                                + aMapLocation.getErrorInfo());
+                    }
+                }
+            }
+        };
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
 
-        for (int i = 0; i < 100; i++) {
-            orderAdapter.add("order", i);
+        AMapLocationClientOption option = new AMapLocationClientOption();
+        option.setInterval(1000);
+        if (null != mLocationClient) {
+            mLocationClient.setLocationOption(option);
+            //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+            mLocationClient.stopLocation();
+            mLocationClient.startLocation();
         }
 
+
+
+
+//        recyclerView = findViewById(R.id.order);
+//        recyclerView.setItemAnimator(new SlideInUpAnimator());
+//        orderAdapter = new OrderAdapter(this, new CopyOnWriteArrayList<String>());
+//        ScaleInAnimationAdapter animationAdapter = new ScaleInAnimationAdapter(orderAdapter);
+//        animationAdapter.setFirstOnly(false);
+//        recyclerView.setAdapter(animationAdapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        for (int i = 0; i < 100; i++) {
+//            orderAdapter.add("order", i);
+//        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stopLocation();
+        mLocationClient.onDestroy();
     }
 }
